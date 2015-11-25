8c97076cf47a717993103572b93fa8c5bb08f54d
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-11-25 14:53:12 -0300

    Arreglo menor.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   babel: Ember.inject.service(),
+  code: "alert('hola?');",
+  lastError: "",
 
   actions: {
     run() {
-      var sourceCode = $('#code').val();
+      var code = this.get("code");
 
-      this.get('babel').run(sourceCode);
+      try {
+        this.get("babel").run(code);
+        this.set("lastError", "");
+      } catch (err) {
+        this.set("lastError", err);
+      }
+
+    },
+
+    close() {
+      this.set("lastError", "");
     }
   }
 });
--- app/services/babel.js
+++ app/services/babel.js
@@ -2,105 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 
-  run(sourceCode) {
-    alert(sourceCode);
+  _eval(code) {
+    eval(code);
+  },
 
-    /*
+  run(code) {
+    var transformed;
 
-    var runButton = document.getElementById("runButton");
-    var codeElement = document.getElementById("code");
+    try {
+      transformed = babel.transform(code, {stage: 2, loose: true, filename: 'repl'});
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
 
-
-    function evaluate(code) {
-      alert(code);
-    };
-
-    var reevaluate = function(code) {
-        capturingConsole = Object.create(console);
-        var $consoleReporter = this.$consoleReporter;
-        var buffer = [];
-        var error;
-        var done = false;
-
-        function flush() {
-          $consoleReporter.text(buffer.join('\n'));
-        }
-
-        function write(data) {
-          buffer.push(data);
-          if (done) flush();
-        }
-
-        capturingConsole.clear = function() {
-          buffer = [];
-          flush();
-        };
-
-        capturingConsole.error = function () {
-          error = true;
-          capturingConsole.log.apply(capturingConsole, arguments);
-        };
-
-        capturingConsole.log =
-        capturingConsole.info =
-        capturingConsole.debug = function() {
-          if (this !== capturingConsole) { return; }
-
-          var args = Array.prototype.slice.call(arguments);
-          Function.prototype.apply.call(console.log, console, args);
-
-          var logs = args.reduce(function (logs, log) {
-            if (typeof log === 'string') {
-              logs.push(log);
-            } else if (log instanceof Function) {
-              logs.push(log.toString());
-            } else {
-              logs.push(JSON.stringify(log));
-            }
-            return logs;
-          }, []);
-
-          write(logs.join(' '));
-        };
-
-        try {
-          new Function('console', code)(capturingConsole);
-        } catch (err) {
-          error = err;
-          buffer.push(err.message);
-        }
-
-        done = true;
-        flush();
-
-        if (error) throw error;
-      };
-
-
-
-
-    runButton.onclick = function() {
-        var transformed;
-        var code = codeElement.value;
-        //this.clearOutput();
-
-        try {
-          transformed = babel.transform(code, {
-            stage: 2,
-            loose: true,
-            filename: 'repl'
-          });
-        } catch (err) {
-          alert(err);
-          //this.printError(err.message);
-          throw err;
-        }
-
-        //this.setOutput(transformed.code);
-
-        evaluate(transformed.code);
-    };
-    */
+    this._eval(transformed.code);
   }
-  
+
 });
